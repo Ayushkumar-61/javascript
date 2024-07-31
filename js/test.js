535c4459e53d2b28f487161d145a1ae05c6a4f3d
Ayushkumar-61 <146440784+Ayushkumar-61@users.noreply.github.com>
2024-07-31 16:08:42 +0000

    done

--- js/test.js
+++ js/test.js
@@ -1,1 +1,16 @@
-console.log("Ayush")
+const cars1 = "maruti suzuki"
+// const cars2 = "SUV"
+// console.log(cars.blink());
+// console.log(cars.anchor('ayush'));
+// console.log(cars.at(3));
+// console.log(cars.big());
+// console.log(cars.bold());
+// console.log(cars.charCodeAt());
+// console.log(cars.codePointAt());
+// console.log(cars.codePointAt(3));
+// console.log(cars1.concat( cars2));
+// console.log(cars2.concat( cars1));
+// console.log(cars1.endsWith("suzuki"));
+// console.log(cars1.fixed());
+console.log(cars1.isWellFormed());
+
