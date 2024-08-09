4ed01113eab61636efe6a425680ad7bef8fe6be9
Ayushkumar-61 <146440784+Ayushkumar-61@users.noreply.github.com>
2024-08-09 08:09:46 +0000

    done

--- js/test.js
+++ js/test.js
@@ -480,11 +480,37 @@ function loginusermessage(username= "sam"){
 
 // console.log("result:",result);
 
-console.log(loginusermessage("ayush"))
+// console.log(loginusermessage("ayush"))
+
+function calculatecartPrice(val1, val2, ...num1){
+     return num1;
+}
+// console.log(calculatecartPrice(200, 400, 500, 2000))
 
 
+// const user = {
+//    username: "ayush",
+//    price: 199
+// }
 
+// function handleObject(anyobject){
+//     console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`)
+// }
 
+// handleObject(user)
+// handleObject({
+//    username: "ayushkumar",
+//    price: 300
+// })
+
+// const myNewArray = [200, 400, 100, 600]
+
+function returnSecondValue(getArray){
+   return getArray[1]
+}
+
+// console.log(returnSecondValue(myNewArray))
+//   console.log(returnSecondValue([200, 400, 100, 600]))
 
 
 
