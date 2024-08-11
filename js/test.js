219dd640e864a78cf1967c94fddefb07edda6626
Ayushkumar-61 <146440784+Ayushkumar-61@users.noreply.github.com>
2024-08-11 08:42:22 +0000

    done

--- js/test.js
+++ js/test.js
@@ -538,8 +538,43 @@ if(true){
 // console.log(c);
 
 
+function one(){
+   const username = "ayush"
+
+   function two(){
+      const website = "youtube"
+       console.log(username);
+   }
+   //console.log(website);
+    
+    //two()
+}
+
+ //one()
 
 
+// if(true){
+// const username = "ayush"
+// if(username === "ayush"){
+//  const website = " youtube"
+//  console.log(username + website);
+// }
+// //console.log(website);
+// }
+
+//console.log(username);
+
+
+console.log(addone(5));
+function addone(num){
+   return num + 1;
+}
+
+
+console.log(addTwo(5));
+const addTwo = function(num){
+   return num + 2;
+}
 
 
 
