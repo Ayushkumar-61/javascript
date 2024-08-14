0f574a06aecfa75e7350399d1cc926f3f1555255
Ayushkumar-61 <146440784+Ayushkumar-61@users.noreply.github.com>
2024-08-14 17:12:56 +0000

    done

--- js/test.js
+++ js/test.js
@@ -670,26 +670,98 @@
 
 // ********************** short-hand condition check ******************* //
 
-const balance = 1000
+// const balance = 1000
 
 // if (balance > 500) console.log("test");     // implect scope //
 
 
-if (balance < 500){
-   console.log("less than 500");
-}else if (balance < 750){
-       console.log("less than 750");
-}else if(balance < 950){
-   console.log("less than 950");
-}else{
-   console.log("less than 1200");
-}
+// if (balance < 500){
+//    console.log("less than 500");
+// }else if (balance < 750){
+//        console.log("less than 750");
+// }else if(balance < 950){
+//    console.log("less than 950");
+// }else{
+//    console.log("less than 1200");
+// }
 
 
 
+// const isUserLoggedIn = true
+// const debitcard = true
+// const LoggedInfromGoogle = false
+// const LoggedInfromEmail = true
+// const guesuser = true
+
+// if(isUserLoggedIn && debitcard && 2==3){
+//    console.log("Allow to buy course")
+// }
+
+// if (LoggedInfromGoogle || LoggedInfromEmail || guesuser){
+//    console.log("user logged in");
+// }
 
 
+// const month = "march"
 
+// switch (month) {
+//    case "jan":
+//       console.log("jan");
+//       break;
+//    case "feb":
+//       console.log("feb");
+//       break;
+//    case "march":
+//       console.log("march");
+//        break;
+//    case "april":
+//       console.log("april");
+//       break;
+
+//    default:
+//       console.log("default case match");
+//       break;
+// }
+
+
+// const userEmail = []
+
+// if (userEmail){
+//    console.log("Got user email");
+// }else{
+//    console.log("Dont have user email");
+// }
+
+
+// if (userEmail.length == 0){
+//    console.log("Array is empty");
+// }
+
+
+// const emptyobj = {}
+
+// if (Object.keys(emptyobj).length == 0){
+//    console.log("object is empty");
+// }else{
+//    console.log("not");
+// }
+
+// Nullish coalescing operator (??) null undefined //
+
+let val1;
+// val1 = 5 ?? 10
+//val1 = null ?? 10
+//val1 = undefined ?? 15
+// val1 = null ?? 10 ?? 20
+//console.log(val1);
+
+
+// Ternary operator //
+
+// condition ? true: false
+
+// const iceTeaPrice = 100
+// iceTeaPrice <80 ? console.log("less than 80") : console.log("more than 80");
 
 
 
