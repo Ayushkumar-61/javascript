d3d7128aa58ecddfe6e6cabaa166c815fcac4539
Ayushkumar-61 <146440784+Ayushkumar-61@users.noreply.github.com>
2024-08-18 15:54:35 +0000

    done

--- js/test.js
+++ js/test.js
@@ -915,7 +915,7 @@ let val1;
 //     }
 
 
-const coding  = ["js", "rubby", "java", "python", "cpp"]
+// const coding  = ["js", "rubby", "java", "python", "cpp"]
 
 // coding.forEach(function (item){
 //     console.log(item);
@@ -961,4 +961,122 @@ const coding  = ["js", "rubby", "java", "python", "cpp"]
 
 
 
+// ****************** filters in javascript **************** //
 
+
+// const coding = ["js", "ruby", "java", "python", "cpp"]
+
+// const values = coding.forEach((item) => {
+//         console.log(item);
+//         return item;
+// })
+
+// console.log(values);
+
+
+// const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    
+// const newNums = myNums.filter((num) => num > 4)
+
+// console.log(newNums);
+
+
+// const number = myNums.forEach((num) => num > 4)
+// console.log(myNums);
+
+
+// const newNums = []
+
+// myNums.forEach((num) => {
+//     if (num > 4){
+//         newNums.push(num)
+//     }
+// })
+// console.log(newNums);
+
+
+// const books = [
+//         { title: 'Book One', genre: 'Fiction', publish: 1981, 
+//             edition: 2004},
+//         { title: 'Book Two', genre: 'non-Fiction', publish: 1992, 
+//             edition: 2008},
+//         { title: 'Book Three', genre: 'History', publish: 1999, 
+//             edition: 2007},
+//         { title: 'Book Four', genre: 'non-Fiction', publish: 1989, 
+//             edition: 2010},
+//         { title: 'Book Five', genre: 'Science', publish: 2009, 
+//             edition: 2014},
+//         { title: 'Book Six', genre: 'Fiction', publish: 1987, 
+//             edition: 2010},
+//         { title: 'Book Seven', genre: 'History', publish: 1986, 
+//             edition: 1986},
+//         { title: 'Book Eight', genre: 'Science', publish: 2011, 
+//             edition: 2016},
+//         { title: 'Book Nine', genre: 'non-Fiction', publish: 1981, 
+//             edition: 1989},
+//     ];
+
+
+//      let userbooks =  books.filter((bk) => bk.genre === 'History')
+// //     console.log(userbooks);
+
+//    userBooks = books.filter((bk) => {
+//         return bk.publish >= 1995 && bk.genre === "History"
+//     });
+
+//     console.log(userbooks);
+
+
+const myNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// const newNums = myNumbers.map((num) => {return num + 10})
+// console.log(newNums);
+// const newNums = [];
+// myNumbers.forEach((num) => {
+//         newNums.push(num + 10)
+//         return newNums;
+// })
+// console.log(newNums);
+
+// const newNums = myNumbers.map((num) => num * 10).map((num) => num + 1).filter((num) => num >= 40)
+
+// console.log(newNums);
+
+
+// reduce method.
+
+
+// const myNums = [1, 2, 3]
+
+//    const mytotal =  myNums.reduce(function (acc, currval){
+//         console.log(`acc ${acc} and currval: ${currval}`);
+//         return acc + currval;
+//     }, 0)
+
+//   const mytotal  = myNums.reduce((acc,currval) => acc+currval, 0)
+
+//     console.log(mytotal);
+
+
+const shoppingcart = [
+        {
+            itemname: "js course",
+            price: 2999
+        },
+        {
+            itemname: "py course",
+            price: 999
+        },
+        {
+            itemname: "mobile dev course",
+            price: 5999
+        },
+        {
+            itemname: "data science course",
+            price: 12999
+        },
+    ]
+
+   const pricetopay =  shoppingcart.reduce((acc,arr) => acc+arr.price, 0)
+
+   console.log(pricetopay);
