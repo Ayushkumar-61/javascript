3d7fa911eb1681cc6fe3f01ed76f926a0b218069
Ayushkumar-61 <146440784+Ayushkumar-61@users.noreply.github.com>
2024-08-29 16:45:01 +0000

    done

---
+++ js/Promise.js
@@ -0,0 +1,80 @@
+// const promiseOne = new Promise(function(resolve, reject){
+//   setTimeout(function(){
+//     console.log('Asyn task is complete');
+//     resolve()
+//   }, 1000)
+// })
+
+
+//  promiseOne.then(function(){
+//      console.log("promises consumed");
+//  })
+
+
+
+// new Promise(function(resolve, reject){
+//     setTimeout(function(){
+//         console.log('Async task 2');
+//         resolve();
+//     }, 1000)
+// }).then(function(){
+//     console.log('Async 2 resolved');
+// })
+
+
+
+// const promiseThree = new Promise(function(resolve, reject){
+//     setTimeout(function(){
+//      resolve({username: "ayush", email: "ayush@gmail.com"})
+//     }, 1000)
+// })
+
+// promiseThree.then(function(user){
+//     console.log(user);
+// })
+
+
+// const PromiseFour = new Promise(function(resolve, reject){
+//     setTimeout(function(){
+//     let error = true;
+//     if(!error){
+//         resolve({username: "ayush", password: "123"})
+//     }else{
+//         reject('ERROR something went wrong')
+//     }
+//     },1000)
+// })
+
+// PromiseFour.then((user) =>{
+//    console.log(user);
+//    return user.username;
+// }).then((username) =>{
+//     console.log(username)
+// }).catch(function(error){
+//     console.log(error)
+// }).finally(()=> console.log("the promises are either resolved or rejected"))
+
+
+const Promise5 = new Promise(function(resolve, reject){
+    setTimeout(function(){
+        let error = false;
+        if(!error){
+            resolve({username: "ayush", password: "123"})
+        }else{
+            reject('ERROR: js went wrong')
+        }
+    }, 1000)
+});
+
+
+async function consumerPromise5(){
+    try {
+        const response = await Promise5
+        console.log(response);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+
+consumerPromise5()
