454662e0ac091c71d0af0dc868b25a1f7d3ae0f8
Ayushkumar-61 <146440784+Ayushkumar-61@users.noreply.github.com>
2024-08-30 08:15:46 +0000

    done

--- js/Promise.js
+++ js/Promise.js
@@ -55,26 +55,51 @@
 // }).finally(()=> console.log("the promises are either resolved or rejected"))
 
 
-const Promise5 = new Promise(function(resolve, reject){
-    setTimeout(function(){
-        let error = false;
-        if(!error){
-            resolve({username: "ayush", password: "123"})
-        }else{
-            reject('ERROR: js went wrong')
-        }
-    }, 1000)
-});
+// const Promise5 = new Promise(function(resolve, reject){
+//     setTimeout(function(){
+//         let error = false;
+//         if(!error){
+//             resolve({username: "ayush", password: "123"})
+//         }else{
+//             reject('ERROR: js went wrong')
+//         }
+//     }, 1000)
+// });
 
 
-async function consumerPromise5(){
-    try {
-        const response = await Promise5
-        console.log(response);
-    } catch (error) {
-        console.log(error);
-    }
-}
+// async function consumerPromise5(){
+//     try {
+//         const response = await Promise5
+//         console.log(response);
+//     } catch (error) {
+//         console.log(error);
+//     }
+// }
 
 
-consumerPromise5()
+// consumerPromise5()
+
+
+// async function getAlluser(){
+//     try {
+//      const response =    await fetch('https://api.github.com/users/Ayushkumar-61')
+//     const data =  await response.json()
+//     console.log(data);
+//     } catch (error) {
+//         console.log("ERORR:", error);
+//     }
+// }
+
+
+// getAlluser()
+
+
+
+// fetch('https://api.github.com/users/Ayushkumar-61')
+// .then((response) => {
+//     return response.json()
+// })
+// .then((data) => {
+//     console.log(data)
+// })
+// .catch((error) => console.log(error));
